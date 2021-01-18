59675d2c16eb3e745e677a37be34b6deff877751
Alex Manusovich <alex.manusovich@opengaragerentals.com>
2021-01-17 22:39:34 -0600

    wip

--- src/VarSpeedServo.cpp
+++ src/VarSpeedServo.cpp
@@ -253,7 +253,7 @@ unsigned int VarSpeedServo::process(unsigned int deltaT)
 
              if (this->step == 8) {
                 // j3
-                this->_AccelStepper.move(7000 * this->direction); // 180deg
+                this->_AccelStepper.move(8000 * this->direction); // 180deg
             }
 
             this->_AccelStepper.setAcceleration(2000);
