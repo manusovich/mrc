e265daff76fd8d6364d1a54d3ddfeb23fd6664b9
Alex Manusovich <alex.manusovich@opengaragerentals.com>
2021-01-17 20:25:21 -0600

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -97,10 +97,6 @@ void setup()
             as->setPinsInverted(true, false, false);
         }
 
-         if (servoConfig[i][0] == 6) {
-            as->setPinsInverted(true, false, false);
-        }
-
         as->setMaxSpeed(3000);
         as->addEncoder(enc, servoConfig[i][10] / servoConfig[i][11]);
 
