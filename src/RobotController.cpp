9b9dcabec6c246d733f829c3262ac229f1995aa6
Alex Manusovich <alex.manusovich@opengaragerentals.com>
2021-01-17 19:57:00 -0600

    wip

--- src/RobotController.cpp
+++ src/RobotController.cpp
@@ -311,9 +311,9 @@ void RobotController::setTargetLogicalAngle(unsigned int index,
 }
 
 void RobotController::runCalibration() {
-        // this->Servos[0]->runCalibration();
+        this->Servos[0]->runCalibration();
         this->Servos[2]->runCalibration();
-        // this->Servos[3]->runCalibration();
+        this->Servos[3]->runCalibration();
        
        
        
